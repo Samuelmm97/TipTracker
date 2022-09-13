b3c6b41233f2970847ca099a4ad964ebe128a8c8
Samuelmm97 <59254672+Samuelmm97@users.noreply.github.com>
2022-09-13 16:48:13 -0400

    cleaned up console logs

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,6 +1,5 @@
 import * as dotenv from "dotenv";
 dotenv.config();
-console.log(process.env.POSTGRES_USER);
 import express from "express";
 import { AuthRequestBody } from "./models/models";
 import { utils } from "./utils/postgres";
@@ -9,15 +8,10 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const port = 2994;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = 3000;
 
 app.post("/register", async (req, res) => {
   const body: AuthRequestBody = req.body;
-  console.log(body);
 
   await utils.registerUser(body);
 
@@ -39,31 +33,3 @@ app.listen(port, async () => {
   await utils.connectDB();
   return console.log(`Express is listening at http://localhost:${port}`);
 });
-
-let wasCleanedUp = false;
-
-const runBeforeExiting = (fun: Function) => {
-  const exitSignals = [
-    "exit",
-    "SIGINT",
-    "SIGUSR1",
-    "SIGUSR2",
-    "uncaughtException",
-  ];
-  for (const signal of exitSignals) {
-    process.on(signal as any, async () => {
-      // eslint-disable-line @typescript-eslint/no-explicit-any
-      if (!wasCleanedUp && signal !== "exit") {
-        await fun();
-        wasCleanedUp = true;
-      }
-      process.exit(0);
-    });
-  }
-};
-
-// runBeforeExiting(async () => {
-//   console.log("server closing...");
-//   await utils.disconnect();
-//   return null;
-// });
