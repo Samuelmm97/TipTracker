f3021b8517e84721253caf9a2c093ff68d6ea3ab
Sam <sammymenaged@yahoo.com>
2022-09-20 15:33:13 -0400

    - switched from ts-postgres package to postgres, - added new models - added onboarding and profile read and update endpoints

--- API/src/app.ts
+++ API/src/app.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-import { AuthRequestBody } from "./models/models";
+import { AuthRequestBody, ProfileReqBody } from "./models/models";
 import { utils } from "./utils/postgres";
 import bodyParser from "body-parser";
 
@@ -29,7 +29,40 @@ app.post("/login", async (req, res) => {
   res.sendStatus(200);
 });
 
+app.post("/onboarding", async (req, res) => {
+  const body: ProfileReqBody = req.body;
+
+  let result = await utils.onboarding(body);
+  if (!result) {
+    res.sendStatus(400);
+    return;
+  }
+  res.sendStatus(200);
+});
+
+app.get("/profile/:userId", async (req, res) => {
+  const { userId } = req.params;
+
+  let result = await utils.getProfile(userId);
+
+  if (!result) {
+    res.sendStatus(400);
+    return;
+  }
+  res.send(JSON.stringify(result));
+});
+
+app.put("/profile/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const { body } = req;
+  let result = await utils.updateProfile(userId, body);
+  if (!result) {
+    res.sendStatus(400);
+    return;
+  }
+  res.sendStatus(200);
+});
+
 app.listen(port, async () => {
-  await utils.connectDB();
   return console.log(`Express is listening at http://localhost:${port}`);
 });
--- API/src/utils/postgres.ts
+++ API/src/utils/postgres.ts
@@ -1,26 +1,19 @@
-import { Client, Query } from "ts-postgres";
-import { AuthRequestBody } from "../models/models";
+import postgres from "postgres";
+import { AuthRequestBody, ProfileReqBody } from "../models/models";
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
 
-const client = new Client({
+const sql = postgres({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: "tippal",
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
+  ssl: true,
 });
 
 export const utils = {
-  connectDB: async () => {
-    await client.connect();
-    return "connected";
-  },
-  disconnect: async () => {
-    await client.end();
-    return "ended";
-  },
   registerUser: async (user: AuthRequestBody) => {
     try {
       bcrypt.hash(user.password, saltRounds, async (err, hash: string) => {
@@ -29,12 +22,9 @@ export const utils = {
           return err;
         }
 
-        const result = await client.query(
-          `INSERT INTO accounts (email, password, created_on, last_login)
-          VALUES ($1, $2, current_timestamp, current_timestamp)`,
-          [user.email, hash]
-        );
-
+        const result =
+          await sql`INSERT INTO accounts (email, password, created_on, last_login)
+          VALUES (${user.email}, ${hash}, current_timestamp, current_timestamp)`;
       });
     } catch (e) {
       console.log("Error inserting into accounts postgres", e);
@@ -43,16 +33,10 @@ export const utils = {
   },
   login: async (user: AuthRequestBody) => {
     try {
-      const result = await client.query(
-        `SELECT password FROM accounts
-         WHERE email = $1`,
-        [user.email]
-      );
+      const result = await sql`SELECT password FROM accounts
+         WHERE email = ${user.email}`;
 
-      if (result.status === "SELECT 0") {
-        return false;
-      }
-      let password = "" + result.rows[0][0];
+      let password = "" + result[0].password;
       let isValid = await new Promise((res, rej) => {
         bcrypt.compare(user.password, password, (err, result) => {
           res(result);
@@ -62,6 +46,41 @@ export const utils = {
       return isValid;
     } catch (e) {
       console.log("Error getting user during login postgres", e);
+      return false;
+    }
+  },
+  onboarding: async (profile: ProfileReqBody) => {
+    try {
+      await sql`insert into profile (employee_type, hours_per_week, work_address, wage, user_id, last_modified)
+         values (${profile.employeeType}, ${profile.hoursPerWeek ?? null}, ${
+        profile.workAddress ?? null
+      }, ${profile.wage}, ${profile.userId}, current_timestamp)`;
+
+      return true;
+    } catch (e) {
+      console.log("Error onboarding postgres", e);
+      return false;
+    }
+  },
+  getProfile: async (userId: string) => {
+    try {
+      const result = await sql`SELECT * FROM profile
+         WHERE user_id = ${userId}`;
+      return result[0];
+    } catch (e) {
+      console.log("Error getting profile postgres", e);
+      return false;
+    }
+  },
+  updateProfile: async (userId: string, profile: any) => {
+    try {
+      const result = await sql`update profile
+                              set ${sql(profile)},
+                              "last_modified" = current_timestamp
+                              WHERE user_id = ${userId}`;
+      return true;
+    } catch (e) {
+      console.log("Error getting profile postgres", e);
       return false;
     }
   },
