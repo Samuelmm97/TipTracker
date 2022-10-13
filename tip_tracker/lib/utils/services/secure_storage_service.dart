491eafe5e7a9789f7a586ead43b9d4c30b75db91
brandongevat <brandongevat@gmail.com>
2022-10-13 11:50:32 -0400

    Update services

--- tip_tracker/lib/utils/services/secure_storage_service.dart
+++ tip_tracker/lib/utils/services/secure_storage_service.dart
@@ -3,42 +3,55 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 class SecureStorageService {
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
 
-  Future<void> _readAll(String accountName) async {
-    await _storage.readAll(
-      iOptions: _getIOSOptions(accountName),
+  Future<Map<String, String>> _readAll() async {
+    return await _storage.readAll(
       aOptions: _getAndroidOptions(),
     );
-  }
-
-  Future<void> deleteAll(accountName) async {
-    await _storage.deleteAll(
-      iOptions: _getIOSOptions(accountName),
-      aOptions: _getAndroidOptions(),
-    );
-    _readAll(accountName);
   }
 
   Future<String?> readItem(String key) async {
-    return await _storage.read(key: key);
+    return await _storage.read(
+      key: key,
+      aOptions: _getAndroidOptions(),
+    );
   }
 
-  Future<void> addNewItem(String accountName, String key, String value) async {
+  Future<void> deleteAll() async {
+    await _storage.deleteAll(
+      aOptions: _getAndroidOptions(),
+    );
+  }
+
+  Future<void> addNewItem(String key, String value) async {
     await _storage.write(
       key: key,
       value: value,
-      iOptions: _getIOSOptions(accountName),
       aOptions: _getAndroidOptions(),
     );
-    _readAll(accountName);
   }
 
-  IOSOptions _getIOSOptions(String accountName) => IOSOptions(
-        accountName: accountName,
-      );
-
+  // Check Android versions for other encryption options.
   AndroidOptions _getAndroidOptions() => const AndroidOptions(
         encryptedSharedPreferences: true,
-        // sharedPreferencesName: 'Test2',
-        // preferencesKeyPrefix: 'Test'
       );
+
+  // Read encrypted JWT tokens currently stored
+  Future<Map<String, String>> readTokens() async {
+    Map<String, String> tokens = {};
+    String? authToken = await readItem("auth-token");
+    String? refreshToken = await readItem("refresh-token");
+    if (authToken != null) {
+      tokens["auth-token"] = authToken;
+    }
+    if (refreshToken != null) {
+      tokens["refresh-token"] = refreshToken;
+    }
+    return tokens;
+  }
+
+  // Encrypt and store JWT tokens
+  Future<void> storeTokens(String authToken, String refreshToken) async {
+    await addNewItem("auth-token", authToken);
+    await addNewItem("refresh-token", refreshToken);
+  }
 }
