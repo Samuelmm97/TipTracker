9cac03a856706dcd4f52dbce04e8dea01c433ef0
brandongevat <brandongevat@gmail.com>
2022-10-27 14:09:58 -0400

    Update refresh-token and add data/message to response payload

--- API/src/utils/postgres.ts
+++ API/src/utils/postgres.ts
@@ -17,7 +17,7 @@ if (!POSTGRES_USER || !POSTGRES_HOST || !POSTGRES_PASSWORD) {
 const client = new Client({
   user: POSTGRES_USER,
   host: POSTGRES_HOST,
-  database: "tippal",
+  database: "tipmate",
   password: POSTGRES_PASSWORD,
   port: 5432,
 });
