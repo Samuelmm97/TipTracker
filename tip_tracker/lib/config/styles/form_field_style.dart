88997fcc7217bcefd9e4414cda00f49f4b2a7b17
brandongevat <brandongevat@gmail.com>
2022-10-28 12:32:31 -0400

    Add form field style

---
+++ tip_tracker/lib/config/styles/form_field_style.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+InputDecoration formFieldStyle = const InputDecoration(
+  fillColor: Color.fromARGB(255, 4, 21, 31),
+  filled: true,
+  border:
+      OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
+  hintText: 'Email',
+  hintStyle: TextStyle(color: Color(0xff3F3B3B)),
+);
