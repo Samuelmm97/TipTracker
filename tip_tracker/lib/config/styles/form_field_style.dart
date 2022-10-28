29d5359b0e723aabbd271e0fa3f5c238f48eba62
brandongevat <brandongevat@gmail.com>
2022-10-28 14:27:19 -0400

    Add reusable styling

--- tip_tracker/lib/config/styles/form_field_style.dart
+++ tip_tracker/lib/config/styles/form_field_style.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-InputDecoration formFieldStyle = const InputDecoration(
-  fillColor: Color.fromARGB(255, 4, 21, 31),
-  filled: true,
-  border:
-      OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(16))),
-  hintText: 'Email',
-  hintStyle: TextStyle(color: Color(0xff3F3B3B)),
-);
+class FormFieldInputDecoration extends InputDecoration {
+  final String hintText;
+  final Color fillColor = Color.fromARGB(255, 4, 21, 31);
+  final filled = true;
+  final border =
+      OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(16)));
+  final hintStyle = TextStyle(color: Color(0xff3F3B3B));
+
+  FormFieldInputDecoration(this.hintText);
+}
+
+TextStyle formFieldTextStyle =
+    const TextStyle(color: Color.fromARGB(190, 253, 253, 253));
